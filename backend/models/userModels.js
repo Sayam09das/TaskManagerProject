bef6fa03d0e166fd0e2206e63a8be5e0f8087284
sayam09das <dassayam2021@gmail.com>
2025-07-24 15:15:00 +0530

    Remove profilePic field from user schema

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -17,10 +17,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    profilePic: {
-        type: String,
-        default: '',
-    }
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
