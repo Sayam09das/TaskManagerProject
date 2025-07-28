180df8c976f778b52ba0ad4f69e895c55481341f
sayam09das <dassayam2021@gmail.com>
2025-07-28 19:02:39 +0530

    Updated somethings

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,7 @@ const protectedRoutes = require('./routes/protectedRoutes');
 
 
 
+
 const allowedOrigins = [
   'http://localhost:5173',
   'https://schedulo-m21t.onrender.com'
@@ -25,11 +26,10 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin like Postman or curl
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('CORS not allowed for this origin'));
     }
   },
   credentials: true
@@ -41,7 +41,7 @@ app.use(cors({
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(helmet());
+
 
 // Connect to MongoDB
 database();
