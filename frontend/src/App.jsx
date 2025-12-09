6ed0b43d6f7d5688f4adb17dd34c3bc8f852a806
sayam09das <dassayam2021@gmail.com>
2025-12-09 18:55:00 +0530

    `Added CSS styles to App component`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import './App.css'
 const App = () => {
   return (
     <div>
+
+      <h1 className='text-amber-300'>Sedulo App</h1>
+      <p className='text-5xl text-red-700'>Welcome to App guys</p>
     </div>
   )
 }
