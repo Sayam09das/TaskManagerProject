32e503dc587d1499d1eda7f1e7f72ad97e55fed1
sayam09das <dassayam2021@gmail.com>
2025-12-09 18:51:19 +0530

    `Removed multiple components and routes from the frontend codebase`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import GetStarted from './components/GetStarted/GetStarted';
-import LoginPage from './components/LoginPage/Login';
-import Signup from './components/Signup/Signup';
-import Schedulo from './components/TaskMange/TaskManager';
-import PrivateRoute from './components/PrivateRoute';
-import PublicRouteWithRedirect from './components/PublicRouteWithRedirect';
-import ForgotPassword from './components/ForgotPassword/ForgotPassword';
-import OtpVerification from './components/OtpVerification/OtpVerification';
-import ConfirmPassword from './components/ConfirmPassword/ConfirmPassword';
+import React from 'react'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PublicRouteWithRedirect>
-              <GetStarted />
-            </PublicRouteWithRedirect>
-          }
-        />
-        <Route path="/auth/login" element={<LoginPage />} />
-        <Route path="/auth/register" element={<Signup />} />
-        <Route path='/auth/forgot-password' element={<ForgotPassword />} />
-        <Route path='/auth/verify-otp' element={<OtpVerification />} />
-        <Route path='/auth/reset-password' element={<ConfirmPassword />} />
-        <Route
-          path="/schedulo"
-          element={
-            <PrivateRoute>
-              <Schedulo />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+    <div>
+    </div>
+  )
+}
 
-export default App;
+export default App
